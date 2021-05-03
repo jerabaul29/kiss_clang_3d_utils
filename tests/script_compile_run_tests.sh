6cd2bb722cfcc3ad3e999ff8d2e9fd92107a0f96
jerabaul29 <jean.rblt@gmail.com>
2021-05-03 22:01:12 +0200

    Fix compilers flags

--- tests/script_compile_run_tests.sh
+++ tests/script_compile_run_tests.sh
@@ -6,7 +6,7 @@
 # consider if moving to some form of make or cmake may be cleaner
 
 # warning flags
-WFLAGS="-pedantic -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wswitch-default -Wundef -Wno-unused -Wconversion -Wduplicated-cond -Wduplicated-branches -Wlogical-op -Wnull-dereference -Wuseless-cast -Wdouble-promotion -fno-common -std=c++1z -Wfloat-conversion -Wno-deprecated-copy"
+WFLAGS="-pedantic -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wswitch-default -Wundef -Wno-unused -Wconversion -Wduplicated-cond -Wduplicated-branches -Wlogical-op -Wnull-dereference -Wuseless-cast -Wdouble-promotion -fno-common -std=c++1z -Wfloat-conversion"
 
 echo " "
 echo "We will run all tests twice:"
